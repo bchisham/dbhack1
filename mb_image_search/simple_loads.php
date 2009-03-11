df48fe4f667d581c80a1f7d62b092f4a76997ea0
Enicospilus <Enicospilus@9b282468-0c64-11de-9036-69e38a880166>
2009-03-11 22:10:00 +0000

    grab otus for searching ids

--- mb_image_search/simple_loads.php
+++ mb_image_search/simple_loads.php
@@ -18,9 +18,22 @@ $nexmb = simplexml_load_file($nex);
  //create image search service query for a name, returns morphbank image ids based on keyworks search
 function imageurl($name){
 	 	 $mb_xml = simplexml_load_file('http://services.morphbank.net/mbd/request?method=search&objecttype=Image&keywords='. $name .'&limit=3&format=id');	
-
-	return $mb_xml;
+				foreach ($mb_xml->id as $mbid) {
+					echo "<img src = http://test.morphbank.net/$mbid.jpg />";
 }
+	}
 
-  			
+function embedmbimages($mb_xml){
+//all the logic for passing diff queries around for getting image urls, using earlier methods
+	
+		}
+
+function returnotus($nex){
+	$returnotunames = otu($nex);
+	foreach ($returnotunames as $array){
+	$mbserviceurl = imageurl($array);
+	//print_r($mbserviceurl);
+		}
+			}
+			
 ?>
