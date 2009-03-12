604e63f08172bd6fd87260bce2bf3f8436e2e99b
gjuggler <gjuggler@9b282468-0c64-11de-9036-69e38a880166>
2009-03-12 13:08:24 +0000

    Update.

--- visualization/www/cgi-bin/random_mess.pl
+++ visualization/www/cgi-bin/random_mess.pl
@@ -26,13 +26,44 @@ sub do_stuff {
     # Turns dictionary entries into tags in the Bio::Phylo object.
     Bio::Greg::PhyloUtils->dicts_to_tags($tree);
 
-    # DO STUFF TO THE TREE HERE!
+    foreach my $node ($tree->get_nodes) {
+	$node->add_tag_value("branch_color",get_random_color());
+	$node->add_tag_value("node_color",get_random_color());
+	$node->add_tag_value("label_color",get_random_color());
 
-    # STOP DOING STUFF TO THE TREE!
+	$node->add_tag_value("branch_size",get_random_size());
+	$node->add_tag_value("node_size",get_random_size());
+	$node->add_tag_value("label_size",get_random_size());
+
+	$node->add_tag_value("node_shape",get_random_shape());
+    }
 
     # Turns any new tags back into dictionary elements, for xml serialization.
     Bio::Greg::PhyloUtils->tags_to_dict($tree);
 }
+
+sub get_random_shape {
+    my @shapes = qw(square triangle x_star oval);
+    my $ind = int(rand(scalar(@shapes)));
+    return $shapes[$ind];
+}
+
+sub get_random_size {
+    my $lo = 0.2;
+    my $hi = 3;
+    return rand($hi-$lo)+$lo;
+}
+
+sub get_random_color {
+    my $lo = 50;
+    my $hi = 255;
+    my $r = int(rand($hi-$lo))+$lo;
+    my $g = int(rand($hi-$lo))+$lo;
+    my $b = int(rand($hi-$lo))+$lo;
+    return sprintf("(%s,%s,%s)",$r,$g,$b);
+}
+
+
 
 # Loads a NeXML file from either the input string, command-line params, or a default file.
 sub load_tree {
@@ -44,7 +75,7 @@ sub load_tree {
     } elsif (defined $ARGV[0]) {
 	$blocks = Bio::Phylo::IO->parse(-file => $ARGV[0], -format => 'nexml');
     } else {
-	$blocks = Bio::Phylo::IO->parse(-file => "/homes/greg/p/dbhack1/txforms/ensembl-rosettes.xml", -format => 'nexml');
+	$blocks = Bio::Phylo::IO->parse(-file => "./fallback.xml", -format => 'nexml');
     }
 
     my $forest = $blocks->[1];
