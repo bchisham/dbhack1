377014e983678324ef60bc0a87645f57817e8673
Enicospilus <Enicospilus@9b282468-0c64-11de-9036-69e38a880166>
2009-03-12 18:12:04 +0000

    search otu

--- trunk/mb_image_search/simple_loads.php
+++ trunk/mb_image_search/simple_loads.php
@@ -1,5 +1,7 @@
 <?php
 
+//example interface/display on website:: http://seltmann.morphbank.net/dbhack1/
+
 //shows entire output from nexml file
 function simple($nex){
 $nexmb = simplexml_load_file($nex);
@@ -15,19 +17,35 @@ $nexmb = simplexml_load_file($nex);
   		return $name;
   			}
   			
+  	function chrs($nex){
+  	//create function to return chrs from xml; could check this against the ontology before uploading.  
+  		}
+  		
+  	 function ann($nex){
+  	 	//function for looking for annotations and return them, would be very useful with external references.
+  	 	}
+  			
  //create image search service query for a name, returns morphbank image ids based on keyworks search
+
 function imageurl($name){
 	 	 $mb_xml = simplexml_load_file('http://services.morphbank.net/mbd/request?method=search&objecttype=Image&keywords='. $name .'&limit=3&format=id');	
-				foreach ($mb_xml->id as $mbid) {
-					echo "<img src = http://test.morphbank.net/$mbid.jpg />";
-}
-	}
+				if (!empty($name)){
+					echo "searching for:  " . $name. ", ";
+				foreach ($mb_xml->keywords as $keywords) {
+					foreach ($mb_xml->id as $mbid){
+						echo '<li style="height: 180px">';
+						echo "<a href=http://test.morphbank.net/$mbid /><img src = http://test.morphbank.net/thumb/$mbid.jpg /><br />";
+						echo "$keywords</td>";
+						echo "</li>";
+					//var_dump($mbid);
+						}//closes first foreach
+				}//closes  second foreach
+			}else{//closes if statement
+					echo "nexml parsed but no image matches were found";
+						}
+		}//closes the function
 
-function embedmbimages($mb_xml){
-//all the logic for passing diff queries around for getting image urls, using earlier methods
-	
-		}
-
+//all the logic for passing diff queries around for getting image urls, using earlier methods; wanted to keep it out of the index
 function returnotus($nex){
 	$returnotunames = otu($nex);
 	foreach ($returnotunames as $array){
