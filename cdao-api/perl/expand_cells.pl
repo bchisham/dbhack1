1ac01194fa403d6807d77b63ad9c94d1ebe2afe8
bchisham <bchisham@9b282468-0c64-11de-9036-69e38a880166>
2009-03-12 02:25:45 +0000

    fixed handling of long strings. fixed char tag recognition problem.

--- cdao-api/perl/expand_cells.pl
+++ cdao-api/perl/expand_cells.pl
@@ -8,25 +8,26 @@ my %state_map;
 my @characters;
 my $states_processed = 0;
 my $lines = 0;
+my $characters_processed = 0;
 while (<STDIN>){
     #this is a state tag. Match the symbols to fill in the hash table.
     if (/\<(?:state|(?:polymorphic|uncertain)_state_set) +id=\"([-_:.?a-zA-Z-0-9]+)\" +symbol=\"([-_:.?a-zA-Z0-9]+)\".*\>/){
-	    #  print "adding id: $1 symbol: $2\n";
 	if ( !(defined($1) and defined($2))){ die "id: \"$1\" symbol: \"$2\"\n"; }
 	$state_map{ $2 } = $1;
 	$states_processed++;
     }
     if (/\<(?:state|(?:polymorphic|uncertain)_state_set) +symbol=\"([-_:.?a-zA-Z-0-9]+)\" +id=\"([-_:.?a-zA-Z0-9]+)\".*\>/){
         if ( !(defined($1) and defined($2))){ die "symbol: \"$1\" id: \"$2\"\n"; }
-#	print "addint symbol: $1 id: $2\n";
 	$state_map{ $1 } = $2;
 	$states_processed++;
     }
-
-    if (/\<char.*id=\"([-_:.?a-zA-Z-0-9]+)\".*\>/){
+    #this is a character definition
+    if (/\<char .*id=\"([-_:.?a-zA-Z-0-9]+)\".*\>/){
 	push @characters, $1;
-	#print "pushed character $2\n";
+	$characters_processed++;
     }
+    
+    #reset things after the character definitions are done
     if (/\<\/characters\>/){
 	    #	print "resetting\n";
 		my %empty_hash;
@@ -34,18 +35,15 @@ while (<STDIN>){
 		%state_map = %empty_hash;
 		@characters = @empty_array;
 		$states_processed = 0;
+		$characters_processed = 0;
     }
+    #match the actual sequence
     if (/\<seq\>/){
-	    #foreach my $key (keys %state_map){
-	    #print "key: $key state: $state_map{$key}\n";
-	    #}
-
 	my $seq = $_;
 	chomp $seq;
        	$seq =~ s/\<seq\>(.+)\<\/seq\>/$1/g;
-	$seq =~ s/^\W+//g;
-
-	#print "<any>after stripping tags seq is \"$seq\"</any>\n";	
+	#strip leading whitespace from the sequence
+	$seq =~ s/^\W+//g;	
         #Case1 the sequence is whitespace delmited
 	my @elements = split(/[^-a-zA-Z0-9.]/,$seq);
 	my $current_character = 0;
@@ -54,25 +52,31 @@ while (<STDIN>){
 	#it will have a size of 1.
 	if (@elements == 1){
 	     @elements = split(//,$elements[0]);
-	     #print "After split there are: @elements elements\n";
 	}
         #Case2 the sequence was white-space delimited
-	#print "Size of characters: \"@characters\"\n";
-	#print "processed $states_processed states\n";
-	if ($states_processed){
+	if (  $states_processed > 0){
 		foreach my $element (@elements) {
-			#print "<any>$element</any>\n";
-			print "\t\t<cell char=\"$characters[$current_character++]\" state=\"$state_map{$element}\"/>\n";  
+			#make there aren't more characters in the sequence than expected
+			#if there are they get truncated out of existance :P
+			if ($current_character < $characters_processed){
+				print "\t\t<!-- Making-up a cell current_character: $current_character character name: $characters[$current_character] state name:$state_map{$element}  -->\n";
+				print "\t\t<cell char=\"$characters[$current_character++]\" state=\"$state_map{$element}\"/>\n"; 
+			}	
 		}
         }
+	#do continuous states. 
 	else {
 		foreach my $element (@elements) {
-			#print "<any>$element</any>\n";
-			print "\t\t<cell char=\"$characters[$current_character++]\" state=\"$element\"/>\n";  
+		        #make sure there aren't more characters in the sequence than expected
+			#if there are then they get truncated out of existance :P
+			if ($current_character < $characters_processed){
+				print "\t\t<cell char=\"$characters[$current_character++]\" state=\"$element\"/>\n";
+			}	
 		}
 	}
 	
     }
+    #otherwise echo output. 
     else {
 	    print $_;
     }
